40c274d3a34aa57b977e3f3dc8c6a37177f36e0c
Saugat Khanal <saugatkhanal100@gmail.com>
2022-07-28 19:27:19 +0545

    win check left

--- main.py
+++ main.py
@@ -13,13 +13,21 @@ class TicTacToe(ps.Player):
             set_of_used_nums = []
         while self.game_start_official:
             set_of_marker = [self.marker1, self.marker2]
-            player_mark_input = int(input("Number to plot : "))
+            set_ok = {self.marker1:self.x_name, self.marker2:self.y_name}
+            player_mark_input = int(input(f"Number to plot {set_ok[set_of_marker[cycle_index]].capitalize()} : "))
+            if player_mark_input not in range(1,10):
+                continue
             if player_mark_input in set_of_used_nums:
                 print("Cant overlap")
                 continue
+
             set_of_used_nums.append(player_mark_input)
             self.board_num[player_mark_input] = set_of_marker[cycle_index]
             self.board_display()
+
+            if len(set_of_used_nums) == 9:
+                print("All Space Filled!!")
+                quit()
 
             if cycle_index <= 0:
                 cycle_index += 1
